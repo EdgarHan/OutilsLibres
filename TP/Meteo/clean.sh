dfa9bd61114c8303ee04855282d4a74c6461bf2d
Jeremy Fix <jeremy.fix@gmail.com>
2015-02-16 16:43:28 +0100

    cleaning the repo

--- TP/Meteo/clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -f Data/*.txt Data/*.tar.gz  raw_images/*.png Images/*.png Videos/*.mp4
--- TP/Meteo/scripts/meteo_get_date.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zenity --forms --title="Vidéo des températures" --text="Période couverte" --add-entry="Date de début (dd/mm/YYYY)" --add-entry="Date de fin (dd/mm/YYYY)" --separator=" "
